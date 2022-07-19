3b5224f53b6ee1bca0a4cae38cfa5e1d5f33a5ff
RedPlay1890 <84314365+RedPlayer1890@users.noreply.github.com>
2022-07-19 16:06:32 -0500

    Modificación de estatus

--- Comandos/admin/realmcstatus.js
+++ Comandos/admin/realmcstatus.js
@@ -1,7 +1,5 @@
 const util = require('minecraft-server-util');
-const {
-    MessageEmbed
-} = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'realmcstatus',
